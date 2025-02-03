8885e8ce40ba08bbeb5fa4184f7be624cf3ef022
SArkadiusz <arkadinio@gmail.com>
2025-02-03 18:51:22 +0100

    add calendar to html_add_resource

--- zasoby/forms.py
+++ zasoby/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Resource
 
 class ResourceForm(forms.ModelForm):
+    date = forms.DateField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'datepicker'}),
+        input_formats=['%Y-%m-%d']
+    )
+
     class Meta:
         model = Resource
         fields = ['name', 'category', 'quantity', 'unit', 'purchase_date', 'expiration_date']
